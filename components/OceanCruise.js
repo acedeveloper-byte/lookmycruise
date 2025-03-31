2d8927116fc4ebad40cb47ff0f3301e17c7d2a5e
acedeveloper-byte <mayank-kumarace@gmail.com>
2025-03-31 21:22:05 +0530

    added changese

--- components/OceanCruise.js
+++ components/OceanCruise.js
@@ -1,15 +1,47 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
-const OceanCruise = (html) => {
+const OceanCruise = ({ html }) => {
   return (
     <Container className="my-5 hightlights" >
-      <Col>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </Col>
 
+
+      {html ? <Col>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </Col> :
+        <>
+          <h2 className="text-center">Chart Your Course with Look My Cruise!</h2>
+
+          <p>
+            Want to turn your cruise dream into reality? Contact Look My Cruise now,
+            and let us be your perfect booking partner for your next voyage. We
+            offer personalized cruise trips so each guest can have the journey of
+            their lifetime. Whether you are looking forward to a luxurious escape or
+            a budget-friendly voyage, we will help you find the ideal cruise for
+            you.
+          </p>
+
+          <Row>
+            <Col md={6}>
+              <ul>
+                <li>Exclusive Deals and Discounts</li>
+                <li>Top-Rated Ships</li>
+                <li>Wide Selection of Destinations
+                </li>
+                <li>Incredible Onboard Amenities</li>
+                <li>24*7 Customer Support</li>
+                <li>Custom Shore Excursions
+                </li>
+              </ul>
+            </Col>
+
+
+          </Row>
+        </>
+      }
     </Container>
   );
 };
 
 export default OceanCruise;
+
