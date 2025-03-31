92cde816c2d9b321b700ce48579849cf0a9197a0
acedeveloper-byte <mayank-kumarace@gmail.com>
2025-03-31 21:35:40 +0530

    added changes

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -10,7 +10,7 @@
 
 // export default page
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "@/components/includes/Header";
 import Footer from "@/components/includes/Footer";
