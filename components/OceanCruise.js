3dfa0aa0e7a98f7f4bcfe812c85664981c104aac
acedeveloper-byte <mayank-kumarace@gmail.com>
2025-03-31 21:17:27 +0530

    added changese

--- components/OceanCruise.js
+++ components/OceanCruise.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
-const OceanCruise = ({ html }) => {
+const OceanCruise = (html) => {
   return (
     <Container className="my-5 hightlights" >
       <Col>
